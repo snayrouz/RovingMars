5ece0ea8ca3abbc3e79ae4475c61ed8ec6768b90
Sam Nayrouz <samuel.nayrouz@gmail.com>
2022-10-11 10:02:49 -0600

    Add error handling to image download AsyncImage func

--- sampProj/RovingMars/Views/LatestView.swift
+++ sampProj/RovingMars/Views/LatestView.swift
@@ -4,14 +4,28 @@ struct LatestView: View {
   var body: some View {
     NavigationView {
       ZStack {
-        AsyncImage(
-          url: URL(string: "https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/03373/opgs/edr/ncam/NRB_696919762EDR_S0930000NCAM00594M_.JPG")
-        ) { image in
-          image
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-        } placeholder: {
-          MarsProgressView()
+        AsyncImage(url: URL(string: "https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/03373/opgs/edr/ncam/NRB_696919762EDR_S0930000NCAM00594M_.JPG")
+        ) { phase in
+          switch phase {
+            // 1
+          case .empty:
+            MarsProgressView()
+            // 2
+          case .success(let image):
+            image
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+          case .failure(let error):
+            VStack {
+              Image(systemName: "exclamationmark.triangle.fill")
+                .foregroundColor(.orange)
+              Text(error.localizedDescription)
+                .font(.caption)
+                .multilineTextAlignment(.center)
+            }
+          @unknown default:
+            EmptyView()
+          }
         }
       }
       .navigationTitle("Latest Photos")
