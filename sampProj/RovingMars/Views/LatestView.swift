634c9ca6253531d69a21cfa8cf6edb898f1e7622
Sam Nayrouz <samuel.nayrouz@gmail.com>
2022-10-11 09:45:24 -0600

    Add AsyncImage func

--- sampProj/RovingMars/Views/LatestView.swift
+++ sampProj/RovingMars/Views/LatestView.swift
@@ -4,7 +4,15 @@ struct LatestView: View {
   var body: some View {
     NavigationView {
       ZStack {
-        MarsProgressView()
+        AsyncImage(
+          url: URL(string: "https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/03373/opgs/edr/ncam/NRB_696919762EDR_S0930000NCAM00594M_.JPG")
+        ) { image in
+          image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        } placeholder: {
+          MarsProgressView()
+        }
       }
       .navigationTitle("Latest Photos")
     }
